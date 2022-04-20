b8bee1115c7120657a56b2308b223ca7e9536adb
Mohamed amine Naimi <mnaimi@e2r10p14.1337.ma>
2022-04-20 01:47:35 +0000

    Update: just changed the order of some function prototypes in the header file

--- libft.h
+++ libft.h
@@ -65,14 +65,14 @@ char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
-int		ft_lstsize(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+int		ft_lstsize(t_list *lst);
 
 void	*ft_gb_calloc(size_t count, size_t size, t_list **gb_lst);
 char	*ft_strcpy(char *dest, const char *src);
